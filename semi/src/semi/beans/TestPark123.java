903e2fd37d4adea86a96192c18f932f8a7ff111f
user1 <user1@DESKTOP-AC3QINP>
2021-05-24 15:39:28 +0900

    테스트 파일들 초기화 (05/24 작성자 : main)

--- semi/src/semi/beans/TestPark123.java
+++
@@ -1,9 +0,0 @@
-package semi.beans;
-
-public class TestPark123 {
-	public static void main(String[] args) {
-		System.out.println("테스트입니다");
-		System.out.println("브랜치 확인하세요!");
-	}
-
-}
--- semi/src/semi/beans/test01.java
+++
@@ -1,8 +0,0 @@
-package semi.beans;
-
-public class test01 {
-	public static void main(String[] args) {
-		System.out.println("test01");
-		System.out.println("브렌치 테스트");
-	}
-}
