e88e94f2420ba370bff56c7b769de524952bf439
user <user@DESKTOP-H0NI3UH>
2021-05-25 21:54:09 +0900

    회원가입 DAO 강제 주입 (작성자 :정 계진)

--- semi/src/semi/member/beans/MemberDao.java
+++ semi/src/semi/member/beans/MemberDao.java
@@ -9,7 +9,27 @@ import java.sql.PreparedStatement;
 import semi.beans.JDBCUtils;
 
 public class MemberDao {
+	
+	//회원 가입 메소드
+	public void regist(MemberDto memberDto) throws Exception{
 
+			Connection con = JDBCUtils.getConnection();
+			
+			
+			String sql="insert into member values(member_seq.nextval,?,?,?,200000,?)";
+			
+			PreparedStatement ps= con.prepareStatement(sql);
+			ps.setString(1, memberDto.getMemberId());
+			ps.setString(2, memberDto.getMemberPw());
+			ps.setString(3, memberDto.getMemberNick());
+			ps.setString(4, memberDto.getMemberEmail());
+			
+			ps.execute();
+			
+			con.close();
+			
+		}
+	
 	// 로그인 메소드
 	public MemberDto login(MemberDto memberDto) throws Exception {
 		
