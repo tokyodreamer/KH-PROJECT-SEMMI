53dd0e03bee2368c7ed015d4a15cedd4a6abf6ba
user <user@DESKTOP-H0NI3UH>
2021-05-25 21:09:15 +0900

    temp 강미진

--- semi/WebContent/member/memberLogin.jsp
+++ semi/WebContent/member/memberLogin.jsp
@@ -1,7 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
 
-<jsp:include page="/template/header.jsp"></jsp:include>`
+<jsp:include page="/template/header.jsp"></jsp:include>
 <div class="container-600">
 	<div class="row">
 		<h1>오늘도 시-작!</h1>
--- semi/src/semi/member/beans/MemberDao.java
+++ semi/src/semi/member/beans/MemberDao.java
@@ -9,59 +9,30 @@ import java.sql.PreparedStatement;
 import semi.beans.JDBCUtils;
 
 public class MemberDao {
-	
-	
-	//회원 가입 메소드
-	public void regist(MemberDto memberDto) throws Exception{
+
+	// 로그인 메소드
+	public MemberDto login(MemberDto memberDto) throws Exception {
+		
 		Connection con = JDBCUtils.getConnection();
-		
-		String sql="insert into member values(member_seq.nextval,?,?,?,200000,?)";
-		
-		PreparedStatement ps= con.prepareStatement(sql);
+		String sql = "select * from member where member_id = ? and member_pw = ?";
+		PreparedStatement ps = con.prepareStatement(sql);
 		ps.setString(1, memberDto.getMemberId());
 		ps.setString(2, memberDto.getMemberPw());
-		ps.setString(3, memberDto.getMemberNick());
-		ps.setString(4, memberDto.getMemberEmail());
+		ResultSet rs = ps.executeQuery(); 
 		
-		ps.execute();
-		
-		con.close();
-	}
-	
-	// ?
-	public MemberDto sth(int memberNo) throws Exception {
-		Connection con = JDBCUtils.getConnection();
-		
-		String sql = "select * from member where member_no = ?";
-		PreparedStatement ps = con.prepareStatement(sql);
-		ps.setInt(1, memberNo);
-		ResultSet rs = ps.executeQuery();
-		
-		MemberDto memberDto;
+		MemberDto sth;
 		if(rs.next()) {
-			memberDto = new MemberDto();
+			sth = new MemberDto();
 			
-			memberDto.setMemberNo(rs.getInt("member_no"));
-			memberDto.setMemberId(rs.getString("member_id"));
-			memberDto.setMemberPw(rs.getString("member_pw"));
-			memberDto.setMemberNick(rs.getString("member_nick"));
-			memberDto.setMemberPoint(rs.getInt("member_point"));
-			memberDto.setMemberEmail(rs.getString("member_email"));
-			// memberDto.setMemberGrade(rs.getString("member_grade"));
+			sth.setMemberId(rs.getString("member_id"));
+			sth.setMemberPw(rs.getString("member_pw"));
 		}
 		else {
-			memberDto = null;
+			sth = null;
 		}
-		
 		con.close();
 		
-		return memberDto;
+		return sth;
 	}
-	
-	// 로그인 메소드
-	public MemberDto login(MemberDto memberDto) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-}
 
+}
