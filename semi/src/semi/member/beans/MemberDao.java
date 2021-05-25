b14f0bb00445b5f52a596faad0363a5ab9b9316e
Park <Park@DESKTOP-00MH2JR>
2021-05-25 11:57:22 +0900

    MemberDao (박준영)

--- semi/src/semi/member/beans/MemberDao.java
+++ semi/src/semi/member/beans/MemberDao.java
@@ -1,5 +1,32 @@
 package semi.member.beans;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import semi.beans.JDBCUtils;
+
 public class MemberDao {
+	
+	public static final String USERNAME = "semi";
+	public static final String PASSWORD = "semi";
+	
+	//회원 가입 메소드
+	
+	public void regist(MemberDto memberDto) throws Exception{
+		Connection con = JDBCUtils.getConnection();
+		
+		String sql="insert into member values(member_seq.nextval,?,?,?,20000,?)";
+		
+		PreparedStatement ps= con.prepareStatement(sql);
+		ps.setString(1, memberDto.getMemberId());
+		ps.setString(2, memberDto.getMemberPw());
+		ps.setString(3, memberDto.getMemberNick());
+		ps.setString(4, memberDto.getMemberEmail());
+		
+		ps.execute();
+		
+		con.close();
+		
+	}
 
 }
