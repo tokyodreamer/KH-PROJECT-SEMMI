987fc7572f81a1885da2310aabf4520745a74728
yonghyeon0223 <yonghyeon0223@gmail.com>
2021-05-25 10:49:01 +0900

    Admin Dao Dto 수정

--- semi/src/semi/admin/beans/AdminDto.java
+++ semi/src/semi/admin/beans/AdminDto.java
@@ -2,4 +2,39 @@ package semi.admin.beans;
 
 public class AdminDto {
 
+	private int adminNo;
+	private String adminId;
+	private String adminPw;
+	private String adminNick;
+	
+	
+	public int getAdminNo() {
+		return adminNo;
+	}
+	public void setAdminNo(int adminNo) {
+		this.adminNo = adminNo;
+	}
+	public String getAdminId() {
+		return adminId;
+	}
+	public void setAdminId(String adminId) {
+		this.adminId = adminId;
+	}
+	public String getAdminPw() {
+		return adminPw;
+	}
+	public void setAdminPw(String adminPw) {
+		this.adminPw = adminPw;
+	}
+	public String getAdminNick() {
+		return adminNick;
+	}
+	public void setAdminNick(String adminNick) {
+		this.adminNick = adminNick;
+	}
+	public AdminDto() {
+		super();
+	}
+
+	
 }
