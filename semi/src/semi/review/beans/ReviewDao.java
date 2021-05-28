9d73fd47402c02b679cbec6a7f536623ae9dd472
Park <Park@DESKTOP-00MH2JR>
2021-05-28 16:18:16 +0900

    리뷰Dao(리뷰작성메소드) (05/28 작성자:박준영)

--- semi/src/semi/review/beans/ReviewDao.java
+++ semi/src/semi/review/beans/ReviewDao.java
@@ -1,9 +1,35 @@
 package semi.review.beans;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import semi.beans.JDBCUtils;
 
 public class ReviewDao {
 //	public List<ReviewDto> list() throws Exception{
 
 //	}
+	
+	//리뷰 작성 기능??(insert-void)
+	public void write(ReviewDto reviewDto) throws Exception{
+		
+		Connection con = JDBCUtils.getConnection();
+		
+		String sql="insert into review values(review_seq.nextval,?,sysdate,?,?)";
+		PreparedStatement ps=con.prepareStatement(sql);
+		ps.setInt(1, reviewDto.getReviewNick());
+		ps.setString(2, reviewDto.getReviewStar());
+		ps.setString(3, reviewDto.getReviewContent());
+		ps.execute();
+		
+		con.close();
+		
+		
+		
+		
+		
+	}
+	
+	
+	
 }
